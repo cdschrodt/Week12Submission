fe643468f17b31ef3e2ab307e1c7a564e1143ed2
cdschrodt <cdschrodt@mymail.mines.edu>
2019-11-19 10:36:55 -0700

    LoadFile

--- Week12.py
+++ Week12.py
@@ -6,3 +6,13 @@
 def PrintOutput(user_input): #WORKS
     print('OUTPUT', user_input)
 
+def LoadFile(file): #WORKS
+    data = open(file, 'r')
+    newdata = data.readlines()
+    data.close()
+    datalist = []
+    for i in newdata:
+        newlist = i.split()
+        for j in newlist:
+            datalist.append(j)
+    return datalist
